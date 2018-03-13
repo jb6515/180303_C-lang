7b5cd9f6aafdfdc45d91db6c50bf625d8ceaafa9
32151675 <32151675@dankook.ac.kr>
2018-03-13 21:50:32 +0900

    update 180313 21:50_add student complete

--- DB project1/DB project1/source.c
+++ DB project1/DB project1/source.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 #pragma warning(disable: 4996)
-
+int size = 0;
 void menu(void);
 void add_student(void);
 void add_course(void);
@@ -28,7 +28,7 @@ int main(void)
 
 	}
 
-	system("puase");
+	system("pause");
 	return 0;
 }
 
@@ -40,25 +40,30 @@ void menu(void)
 	printf("2. 과목 테이블 작성\n");
 	printf("3. 수강 테이블 작성\n");
 	printf("4. 작성한 데이터 불러오기\n");
+	printf("5. 종료\n");
 }
 
 void add_student(void)
 {
 	int i = 0;
-	int j = 0;
 	struct student_info
 	{
 		char stuname[10];
 		char address[20];
 		char major[10];
-	}student[4];
+	};
+	typedef struct student_info data;
+
+	data student[4];
+
+	system("cls");
 
 	printf("입력할 학생 수를 입력하세요 (최대 5명)\n");
-	scanf("%d", &j);
-	while (getchar() != '\n');
+	scanf("%d", &size);
+	fflush(stdin);
 
 
-	for (i = 0; i < j; i++)
+	for (i = 0; i < size; i++)
 	{
 		printf("%d번째 학생 이름을 입력 : ", i + 1);
 		scanf("%s", student[i].stuname);
@@ -66,26 +71,59 @@ void add_student(void)
 		scanf("%s", student[i].address);
 		printf("%d번째 학생 학과를 입력 : ", i + 1);
 		scanf("%s", student[i].major);
-		while (getchar() != '\n');
+		fflush(stdin);
 	}
 
-	printf("이름 주소 학과");
-	for (i = 0; i < j; i++)
+	printf("이름 주소 학과\n");
+	for (i = 0; i < size; i++)
 	{
 		printf("%s %s %s\n", student[i].stuname, student[i].address, student[i].major);
 	}
+	FILE *fp = fopen("lsd_student.dat", "wb");
+	if (fp == NULL)
+		return;
+	fwrite(&size, sizeof(int), 1, fp);
+	fwrite(&student, sizeof(data), size, fp);
+	fclose(fp);
+
+	system("pause");
 }
 
 
 void add_course(void)
 {
-	return 0;
+	
 }
 void add_course_taken(void)
 {
-	return 0;
+	
 }
 void view(void)
 {
-	return 0;
+	int i = 0;
+	struct student_info
+	{
+		char stuname[10];
+		char address[20];
+		char major[10];
+	};
+	typedef struct student_info data;
+
+	data student[4];
+
+	FILE *fp;
+
+	fp = fopen("lsd_student.dat", "rb");
+	if (fp == NULL)
+		return;
+	fread(&size, sizeof(int), 1, fp);
+	fread(student, sizeof(data), size, fp);
+
+
+	printf("이름 주소 학과\n");
+	for (i = 0; i < size; i++)
+	{
+		printf("%s %s %s\n", student[i].stuname, student[i].address, student[i].major);
+	}
+
 }
