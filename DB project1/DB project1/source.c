8ab57469f18dff2021e5b0c30f3b36ac8995c00f
32151675 <32151675@dankook.ac.kr>
2018-03-14 12:10:27 +0900

    update 180314 12:10

--- DB project1/DB project1/source.c
+++ DB project1/DB project1/source.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-
+#include <string.h>
 #pragma warning(disable: 4996)
-int size = 0;
+int size = 0; // 구조체 개수만큼 파일에 기록하기 위해 선언, 이진파일 첫 부분에 저장됨
 void menu(void);
 void add_student(void);
 void add_course(void);
@@ -13,10 +13,10 @@ int main(void)
 	char choice;
 	while (1)
 	{
-		menu();
-		choice = getch();
-		fflush(stdin);
-		switch (choice)
+		menu(); // 메뉴텍스트
+		choice = getch(); // 메뉴를 입력받음
+		fflush(stdin); // 버퍼 초기화
+		switch (choice) //switch문을 사용하여 입력받은 숫자 처리
 		{
 		case'1':	add_student();			break;
 		case'2':	add_course();			break;
@@ -28,8 +28,7 @@ int main(void)
 
 	}
 
-	system("pause");
-	return 0;
+
 }
 
 
@@ -45,45 +44,40 @@ void menu(void)
 
 void add_student(void)
 {
-	int i = 0;
-	struct student_info
+	int i = 0; // for문 사용을 위해 선언
+	struct student_info // 구조체 만들기
 	{
-		char stuname[10];
-		char address[20];
-		char major[10];
+		char stuid[20];
+		char stuname[20];
+		char major[20];
 	};
-	typedef struct student_info data;
+	typedef struct student_info data_s; // 자료형 재정의
 
-	data student[4];
+	data_s student[5]; // 구조체 배열 선언
 
-	system("cls");
+	system("cls"); // 화면 비우기
 
 	printf("입력할 학생 수를 입력하세요 (최대 5명)\n");
-	scanf("%d", &size);
+	scanf("%d", &size); // for문에서 크기만큼 입력받음 또한, 파일 저장시에 맨 앞에 기록해서 파일 로드시에 size만큼만 불러옴
 	fflush(stdin);
-
-
 	for (i = 0; i < size; i++)
 	{
-		printf("%d번째 학생 이름을 입력 : ", i + 1);
+		printf("%d번째 학생의 학번을 입력 : ", i + 1);
+		scanf("%s", student[i].stuid);
+		printf("%d번째 학생의 이름을 입력 : ", i + 1);
 		scanf("%s", student[i].stuname);
-		printf("%d번째 학생 주소를 입력 : ", i + 1);
-		scanf("%s", student[i].address);
-		printf("%d번째 학생 학과를 입력 : ", i + 1);
+		printf("%d번째 학생의 학과를 입력 : ", i + 1);
 		scanf("%s", student[i].major);
 		fflush(stdin);
 	}
 
-	printf("이름 주소 학과\n");
-	for (i = 0; i < size; i++)
-	{
-		printf("%s %s %s\n", student[i].stuname, student[i].address, student[i].major);
-	}
 	FILE *fp = fopen("lsd_student.dat", "wb");
 	if (fp == NULL)
 		return;
 	fwrite(&size, sizeof(int), 1, fp);
-	fwrite(&student, sizeof(data), size, fp);
+	fwrite(student, sizeof(data_s), size, fp);
+	
+	
 	fclose(fp);
 
 	system("pause");
@@ -92,38 +86,190 @@ void add_student(void)
 
 void add_course(void)
 {
-	
+	int i = 0; // for문 사용을 위해 선언
+	struct course_info // 구조체 만들기
+	{
+		char cou_no[20];
+		char cou_name[20];
+		char professor[20];
+	};
+	typedef struct course_info data_c; // 자료형 재정의
+
+	data_c course[5]; // 구조체 배열 선언
+
+	system("cls"); // 화면 비우기
+
+	printf("입력할 과목 수를 입력하세요 (최대 5개)\n");
+	scanf("%d", &size); // for문에서 크기만큼 입력받음 또한, 파일 저장시에 맨 앞에 기록해서 파일 로드시에 size만큼만 불러옴
+	fflush(stdin);
+
+
+	for (i = 0; i < size; i++)
+	{
+		printf("%d번째 과목의 교과목번호를 입력 : ", i + 1);
+		scanf("%s", course[i].cou_no);
+		printf("%d번째 과목의 이름을 입력 : ", i + 1);
+		scanf("%s", course[i].cou_name);
+		printf("%d번째 과목의 교수를 입력 : ", i + 1);
+		scanf("%s", course[i].professor);
+		fflush(stdin);
+	}
+
+	printf("이름 주소 학과\n");
+	for (i = 0; i < size; i++)
+	{
+		printf("%s %s %s\n", course[i].cou_no, course[i].cou_name, course[i].professor);
+	}
+	FILE *fp = fopen("lsd_course.dat", "wb");
+	if (fp == NULL)
+		return;
+	fwrite(&size, sizeof(int), 1, fp);
+	fwrite(course, sizeof(data_c), size, fp);
+	fclose(fp);
+
+	system("pause");
 }
 void add_course_taken(void)
 {
-	
+	int i = 0; // for문 사용을 위해 선언
+	struct course_taken_info // 구조체 만들기
+	{
+		char stuid[20];
+		char cou_no[20];
+		char grade[20];
+	};
+	typedef struct course_taken_info data_ct; // 자료형 재정의
+
+	data_ct course_taken[5]; // 구조체 배열 선언
+
+	system("cls"); // 화면 비우기
+
+	printf("입력할 수강데이터의 수를 입력하세요 (최대 5개)\n");
+	scanf("%d", &size); // for문에서 크기만큼 입력받음 또한, 파일 저장시에 맨 앞에 기록해서 파일 로드시에 size만큼만 불러옴
+	fflush(stdin);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("%d번째 수강데이터의 학번을 입력 : ", i + 1);
+		scanf("%s", course_taken[i].stuid);
+		printf("%d번째 수강데이터의 교과목번호를 입력 : ", i + 1);
+		scanf("%s", course_taken[i].cou_no);
+		printf("%d번째 수강데이터의 학점을 입력 : ", i + 1);
+		scanf("%s", course_taken[i].grade);
+		fflush(stdin);
+	}
+
+	printf("학번 교과목번호 학점\n");
+	for (i = 0; i < size; i++)
+	{
+		printf("%s %s %s\n", course_taken[i].stuid, course_taken[i].cou_no, course_taken[i].grade);
+	}
+	FILE *fp = fopen("lsd_coursetaken.dat", "wb");
+	if (fp == NULL)
+		return;
+	fwrite(&size, sizeof(int), 1, fp);
+	fwrite(course_taken, sizeof(data_ct), size, fp);
+	fclose(fp);
+
+	system("pause");
+
 }
 void view(void)
 {
 	int i = 0;
+	int j = 0;
+	int n = 0;
+	int size1 = 0;
+	int size2 = 0;
+	int size3 = 0;
 	struct student_info
 	{
-		char stuname[10];
-		char address[20];
-		char major[10];
+		char stuid[20];
+		char stuname[20];
+		char major[20];
 	};
-	typedef struct student_info data;
+	typedef struct student_info data_s;
 
-	data student[4];
+	data_s student[5];
+
+	struct course_info // 구조체 만들기
+	{
+		char cou_no[20];
+		char cou_name[20];
+		char professor[20];
+	};
+	typedef struct course_info data_c; // 자료형 재정의
+
+	data_c course[5]; // 구조체 배열 선언
+
+	struct course_taken_info // 구조체 만들기
+	{
+		char stuid[20];
+		char cou_no[20];
+		char grade[20];
+	};
+	typedef struct course_taken_info data_ct; // 자료형 재정의
+
+	data_ct course_taken[5]; // 구조체 배열 선언
 
 	FILE *fp;
 
 	fp = fopen("lsd_student.dat", "rb");
 	if (fp == NULL)
 		return;
-	fread(&size, sizeof(int), 1, fp);
-	fread(student, sizeof(data), size, fp);
+	fread(&size1, sizeof(int), 1, fp);
+	fread(&student, sizeof(data_s), size1, fp);
+	
+	fclose(fp);
 
+	
+	fp = fopen("lsd_course.dat", "rb");
+	if (fp == NULL)
+		return;
+	fread(&size2, sizeof(int), 1, fp);
+	fread(&course, sizeof(data_c), size2, fp);
 
-	printf("이름 주소 학과\n");
-	for (i = 0; i < size; i++)
+	fclose(fp);
+
+	fp = fopen("lsd_coursetaken.dat", "rb");
+	if (fp == NULL)
+		return;
+	fread(&size3, sizeof(int), 1, fp);
+	fread(&course_taken, sizeof(data_ct), size3, fp);
+
+	fclose(fp);
+
+	for (i = 0; i < size1; i++)
 	{
-		printf("%s %s %s\n", student[i].stuname, student[i].address, student[i].major);
+		printf("%s %s %s\n", student[i].stuid, student[i].stuname, student[i].major);
+	}
+
+	for (i = 0; i < size2; i++)
+	{
+		printf("%s %s %s\n", course[i].cou_no, course[i].cou_name, course[i].professor);
+	}
+
+	for (i = 0; i < size3; i++)
+	{
+		printf("%s %s %s\n", course_taken[i].stuid, course_taken[i].cou_no, course_taken[i].grade);
+	}
+
+	
+	for (n = 0; n < size3; n++)
+	{
+		for (i = 0; i < size1; i++)
+		{
+			if (strcmp(course_taken[n].stuid, student[i].stuid) == 0)
+				printf("%s ", student[i].stuname);
+		}
+
+		for (j = 0; j < size2; j++)
+		{
+			if (strcmp(course_taken[n].cou_no, course[j].cou_no) == 0)
+				printf("%s ", course[j].cou_name);
+		}
+
+		printf("%s \n", course_taken[n].grade);
 	}
 
 }
---
+++ DB project2/DB project2/source.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#pragma warning(disable: 4996)
+
+
+
+int main(void)
+{
+	int i = 0;
+	
+}
